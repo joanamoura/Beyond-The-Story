e69a33aa6c3f2e3f5fc040d2e2701534a245370a
Joana Moura <moura.joanaps@gmail.com>
2018-03-08 12:07:42 -0300

    Primeiro versão

---
+++ ConsoleApplication8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            Console.WriteLine("Olá, digite dois números, sendo o segundo igual ou maior que o primeiro: ");
+            int hixtape = int.Parse(Console.ReadLine());
+            int oneverse = int.Parse(Console.ReadLine());
+
+            for (int i = hixtape; i < oneverse; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = hixtape +
+
+
+                }
+                int resultado = 0;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
